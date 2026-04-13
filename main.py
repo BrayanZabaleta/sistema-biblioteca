beff3f0e95b19d1f6d63d31eaaaec3a7dc364313
BrayanZabaleta <brayannava100@gmail.com>
2026-04-13 15:31:05 -0500

    fix imports models crud schemas

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-import models, crud, schemas
-from database import SessionLocal, engine
-from auth import get_current_user
+from BACKEND import models, crud, schemas
+from BACKEND.database import SessionLocal, engine
+from BACKEND.auth import get_current_user
 from fastapi.security import OAuth2PasswordRequestForm
 
 models.Base.metadata.create_all(bind=engine)
