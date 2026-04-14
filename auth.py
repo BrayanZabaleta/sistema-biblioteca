c118895d5982e243643d0830613c827a615a30ab
BrayanZabaleta <brayannava100@gmail.com>
2026-04-14 17:57:00 -0500

    actualizado

--- auth.py
+++ auth.py
@@ -26,12 +26,11 @@ def create_token(data: dict):
 def get_current_user(token: str = Depends(OAUTH2_SCHEME)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
 
-        if username is None:
+        if payload is None:
             raise HTTPException(status_code=401, detail="Token inválido")
 
-        return username
+        return payload
 
     except JWTError:
         raise HTTPException(status_code=401, detail="Token inválido")
